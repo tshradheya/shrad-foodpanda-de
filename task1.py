b57e8b969fcb22da6d6d6f0cdf68c69518f843f0
Shradheya Thakre <tshradheya@gmail.com>
2021-06-22 19:45:48 +0800

    Cleanup

--- task1.py
+++ task1.py
@@ -8,25 +8,28 @@ And then adds a column which contains distance to this port and saves top 5 clos
 def task1():
 
     df = util.get_dataset_from_bq()
+
     sg_jurong_port = df.loc[(df["country"] == 'SG') & (df['port_name'] == 'JURONG ISLAND')][['port_latitude', 'port_longitude']]
     if sg_jurong_port.shape[0] != 1:
+        print("The mentioned port is not in data")
         return
     lat_port, long_port = float(sg_jurong_port['port_latitude']), float(sg_jurong_port['port_longitude'])
 
+    # Calculate distance and add as column
     distances_arr = []
-
     for row in df.itertuples(index=False):
         distances_arr.append(haversine((lat_port, long_port), (row.port_latitude, row.port_longitude)))
 
     df['distance_in_meters'] = distances_arr
 
-    distance_from_jurnong_island_port = df.where(df['distance_in_meters'] > 0.0).sort_values(by='distance_in_meters').head(5)
-    distance_from_jurnong_island_port = distance_from_jurnong_island_port[['port_name', 'distance_in_meters']]
+    # Get Top 5 excluding that port itself
+    distance_from_jurong_island_port = df.where(df['distance_in_meters'] > 0.0).sort_values(by='distance_in_meters').head(5)
+    distance_from_jurong_island_port = distance_from_jurong_island_port[['port_name', 'distance_in_meters']]
 
-    print(distance_from_jurnong_island_port)
+    print(distance_from_jurong_island_port)
 
     print("Starting writing to table...")
-    util.write_to_bq(distance_from_jurnong_island_port, 'closest_port_to_jurong_island')
+    util.write_to_bq(distance_from_jurong_island_port, 'closest_ports_to_jurong_island')
     print("Finished writing to table")
 
 
--- task3.py
+++ task3.py
@@ -7,18 +7,20 @@ Function which gets closest port based on location and having required resources
 def task3(resources, lat_disaster, long_disaster):
 
     df = util.get_dataset_from_bq()
+    # To filter ports containing all
     for resource in resources:
         df = df[df[resource] == True]
 
     df = df[['country', 'port_name', 'port_latitude', 'port_longitude']]
     suitable_ports = df
-    distances_arr = []
 
+    distances_arr = []
     for row in suitable_ports.itertuples(index=False):
         distances_arr.append(haversine((lat_disaster, long_disaster), (row.port_latitude, row.port_longitude)))
 
     suitable_ports['distance_from_distress'] = distances_arr
 
+    # Get closest port
     closest_port = suitable_ports.sort_values(by='distance_from_distress').head(1)
     closest_port = closest_port[['country', 'port_name', 'port_latitude', 'port_longitude']]
 
